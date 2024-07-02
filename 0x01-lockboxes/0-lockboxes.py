caababfbf494c2c805d0af805476b0a419f04c5b
Clear008 <wissal-exo@hotmail.com>
2024-07-02 23:59:52 +0000

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-    This module contains the canUnlockAll function.
-"""
+"""canUnlockAll function."""
+
 
 def canUnlockAll(boxes):
-    """Method that determines if all the boxes can be opened
-        Arg:
-            boxes: Is a list of lists.
-    """
+    """Method that determines if all the boxes can be opened """
+
     n = len(boxes)
     unlocked = set([0])
     stack = [0]
