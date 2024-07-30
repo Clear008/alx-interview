8fceb53fe984e539a08f397cd3473320ffd89cf4
Clear008 <wissal-exo@hotmail.com>
2024-07-30 14:27:27 +0000

    0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python3
 """ N queens """
-import sys
 from typing import List
-
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
-
-if not sys.argv[1].isdigit():
-    print("N must be a number")
-    sys.exit(1)
-
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-
-n = int(sys.argv[1])
 
 
 class Solution:
