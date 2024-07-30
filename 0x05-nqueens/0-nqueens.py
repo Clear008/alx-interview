0d6b590940fd62e3ef884584f6493727ccc48e53
Clear008 <wissal-exo@hotmail.com>
2024-07-30 14:52:50 +0000

    0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,48 @@
 #!/usr/bin/python3
 """ N queens """
 import sys
-
+from typing import List
 
 if len(sys.argv) > 2 or len(sys.argv) < 2:
     print("Usage: nqueens N")
-    exit(1)
+    sys.exit(1)
 
 if not sys.argv[1].isdigit():
     print("N must be a number")
-    exit(1)
+    sys.exit(1)
 
 if int(sys.argv[1]) < 4:
     print("N must be at least 4")
-    exit(1)
+    sys.exit(1)
 
 n = int(sys.argv[1])
 
 
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-    else:
-        yield a
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        """ N queens function"""
+        cols = set()
+        diagpos = set()
+        diagneg = set()
 
+        res = []
+        board = [['.'] * n for _ in range(n)]
 
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
-
-
-solve(n)
+        def backtrack(row):
+            if row == n:
+                res.append(["".join(row) for row in board])
+                return
+            for c in range(n):
+                if c not in cols and row + c not in diagpos and
+                row - c not in diagneg:
+                    board[row][c] = 'Q'
+                    cols.add(c)
+                    diagpos.add(row + c)
+                    diagneg.add(row - c)
+                    backtrack(row + 1)
+                    board[row][c] = '.'
+                    cols.remove(c)
+                    diagpos.remove(row + c)
+                    diagneg.remove(row - c)
+        backtrack(0)
+        return res
