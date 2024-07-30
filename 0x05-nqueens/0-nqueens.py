92e0b2eba007cb2710efe172a4e7581edb2ce63f
Clear008 <wissal-exo@hotmail.com>
2024-07-30 14:29:59 +0000

    0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,29 +2,43 @@
 """ N queens """
 import sys
 
-def is_safe(board, row, col):
-    """Check this column on upper side"""
-    for i in range(row):
-        if board[i] == col or \
-           board[i] - i == col - row or \
-           board[i] + i == col + row:
-            return False
-    return True
 
-def solve_nqueens(N):
-    """ N queens function"""
-    def solve(board, row):
-        if row == N:
-            solutions.append(board[:])
-            return
-        for col in range(N):
-            if is_safe(board, row, col):
-                board[row] = col
-                solve(board, row + 1)
-                board[row] = -1
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-    solutions = []
-    board = [-1] * N
-    solve(board, 0)
-    return solutions
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
 
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
+n = int(sys.argv[1])
+
+
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
+
+
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(n)
