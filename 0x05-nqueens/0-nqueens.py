e962e9122bfb2ad6bc85def50879d83f208be3a0
Clear008 <wissal-exo@hotmail.com>
2024-07-30 14:56:55 +0000

    0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,44 +5,36 @@ from typing import List
 
 if len(sys.argv) > 2 or len(sys.argv) < 2:
     print("Usage: nqueens N")
-    sys.exit(1)
+    exit(1)
 
 if not sys.argv[1].isdigit():
     print("N must be a number")
-    sys.exit(1)
+    exit(1)
 
 if int(sys.argv[1]) < 4:
     print("N must be at least 4")
-    sys.exit(1)
+    exit(1)
 
 n = int(sys.argv[1])
 
-
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        """ N queens function"""
-        cols = set()
-        diagpos = set()
-        diagneg = set()
-
-        res = []
-        board = [['.'] * n for _ in range(n)]
-
-        def backtrack(row):
-            if row == n:
-                res.append(["".join(row) for row in board])
+        def DFS(queens, xy_dif, xy_sum):
+            p = len(queens)
+            if p == n:
+                result.append(queens)
                 return
-            for c in range(n):
-                if c not in cols and row + c not in diagpos and
-                row - c not in diagneg:
-                    board[row][c] = 'Q'
-                    cols.add(c)
-                    diagpos.add(row + c)
-                    diagneg.add(row - c)
-                    backtrack(row + 1)
-                    board[row][c] = '.'
-                    cols.remove(c)
-                    diagpos.remove(row + c)
-                    diagneg.remove(row - c)
-        backtrack(0)
-        return res
+            for q in range(n):
+                if q not in queens and p-q not in xy_dif and p+q not in xy_sum:
+                    DFS(queens+[q], xy_dif+[p-q], xy_sum+[p+q])
+        result = []
+        DFS([], [], [])
+        return [["."*i + "Q" + "."*(n-i-1) for i in sol] for sol in result]
+
+solution = Solution()
+solutions = solution.solveNQueens(n)
+for sol in solutions:
+    positions = []
+    for row in range(n):
+        positions.append([row, sol[row].index("Q")])
+    print(positions)
